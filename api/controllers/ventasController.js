db521e0e377804027b446b7486feb0c84940aaf9
manuelcastro95 <jmcastro2986@misena.edu.co>
2024-03-17 21:45:18 -0500

    se agrego libreria dataTable react y se implemto en la vista del administrador par alas ventas

--- api/controllers/ventasController.js
+++ api/controllers/ventasController.js
@@ -1,10 +1,21 @@
 const fs = require('fs/promises');
 const path = require('path');
+const rutaArchivo = path.join(__dirname, '../db/ventas.json');
+
+let ventas = [];
+
+async function cargarVentas() {
+  try {
+    const contenido = await fs.readFile(rutaArchivo, 'utf-8');
+    ventas = JSON.parse(contenido);
+  } catch (error) {
+  }
+}
+
+cargarVentas();
 
 const listarVentas = async (req, res) => {
-    res.json({
-      ventas: 'listado de Ventas'
-    });
+    res.json(ventas);
 }
 
 module.exports = {
